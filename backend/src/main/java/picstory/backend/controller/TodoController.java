613a2f267cd649379ba55bc54f7a16e72eea94d0
HSR <hseoglan@naver.com>
2026-03-29 02:06:13 +0900

    1차 업데이트

---
+++ backend/src/main/java/picstory/backend/controller/TodoController.java
@@ -0,0 +1,19 @@
+package picstory.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> getAllTodos(){
+        return ResponseEntity.ok(Collections.emptyList());
+    }
+}
---
+++ frontend/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // server: {
+  //   port: 5174,        // 원하는 포트 번호
+  //   host: '0.0.0.0',   // 컨테이너 안에서 외부 접근 가능하게(npm run dev 사용)
+  // },
+})
